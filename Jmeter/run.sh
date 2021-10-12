23e9965eb6842137a9db77567ea2b09fc83c909e
LQss11 <49565791+LQss11@users.noreply.github.com>
2021-10-12 16:44:38 +0100

    docker jmeter

---
+++ Jmeter/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+#define variables to store info
+version=5.3
+scriptname="PetStoreDemo"
+#
+# override the HEAP settings and run the jmeter script.
+JVM_ARGS="-Xms512m -Xmx2048m" jmeter -Jjmeter.save.saveservice.subresults=false -n -t /${scriptname}.jmx -l /${scriptname}.jtl -e -o /results/output 2>&1
+java -jar /opt/apache-jmeter-${version}/lib/cmdrunner-2.2.jar --tool Reporter --plugin-type AggregateReport --input-jtl /${scriptname}.jtl --generate-csv /results/results.csv 2>&1
+cat /results/results.csv
+sleep 120
