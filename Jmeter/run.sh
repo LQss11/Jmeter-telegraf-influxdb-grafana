82ee1ff442ead4599a8f12ed8429d899f1efc54e
LQss11 <49565791+LQss11@users.noreply.github.com>
2021-12-02 20:15:59 +0100

    persist docker jmeter scripts in volume

--- Jmeter/run.sh
+++ Jmeter/run.sh
@@ -2,10 +2,8 @@
 #define variables to store info
 version=5.3
 scriptname="GoogleTest"
-DN="`date`"
-DATE_NOW=${DN// /_}
-#
-# override the HEAP settings and run the jmeter script.
-JVM_ARGS="-Xms512m -Xmx2048m" jmeter -Jjmeter.save.saveservice.subresults=false -n -t /${scriptname}.jmx -f -l /results/output/$DATE_NOW/${scriptname}.jtl  -e -o /results/output/$DATE_NOW/dashboard -j /results/output/$DATE_NOW/jmeter.log 2>&1
-java -jar /opt/apache-jmeter-${version}/lib/cmdrunner-2.2.jar --tool Reporter --plugin-type AggregateReport --input-jtl /results/output/$DATE_NOW/${scriptname}.jtl --generate-csv /results/results.csv 2>&1
-cat /results/results.csv
+DATE_NOW="`date`"
+#DATE_NOW=${DN// /_}
+JVM_ARGS="-Xms512m -Xmx2048m" jmeter -Jjmeter.save.saveservice.subresults=false -n -t /${scriptname}.jmx -f -l "/results/output/$DATE_NOW/${scriptname}.jtl"  -e -o "/results/output/$DATE_NOW/dashboard" -j "/results/output/$DATE_NOW/jmeter.log" 2>&1
+java -jar /opt/apache-jmeter-${version}/lib/cmdrunner-2.2.jar --tool Reporter --plugin-type AggregateReport --input-jtl "/results/output/$DATE_NOW/${scriptname}.jtl" --generate-csv "/results/output/$DATE_NOW/results.csv" 2>&1
+cat "/results/output/$DATE_NOW/results.csv"
