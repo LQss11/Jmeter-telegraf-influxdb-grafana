5cf1b644576cfdccaac5ae959dd98aff19315d89
LQss11 <49565791+LQss11@users.noreply.github.com>
2022-01-04 05:44:01 +0100

    add MIT LICENCE

--- Jmeter/Scripts/run-dist.sh
+++ Jmeter/Scripts/run-dist.sh
@@ -22,10 +22,10 @@ done
 
 echo "Jmx file= $jmx_file_name.jmx"
 
-
 # Get Date Now for unique test folders names
 DN="$(date)"
 DATE_NOW=${DN// /_}
+
 JVM_ARGS="-Xms512m -Xmx2048m" jmeter  -n -X -Jclient.rmi.localport=7000 -Jserver.rmi.ssl.disable=true -R slave-1,slave-2 -Jjmeter.save.saveservice.subresults=false -t /JMX/${scriptname}.jmx -f -l /results/output/$DATE_NOW/${scriptname}.jtl -e -o /results/output/$DATE_NOW/dashboard -j /results/output/$DATE_NOW/jmeter.log 2>&1
 java -jar /opt/apache-jmeter-${version}/lib/cmdrunner-2.2.jar --tool Reporter --plugin-type AggregateReport --input-jtl /results/output/$DATE_NOW/${scriptname}.jtl --generate-csv /results/output/$DATE_NOW/results.csv 2>&1
 cat /results/output/$DATE_NOW/results.csv
